51fb83a0dcd5731ad9329f3049243e53c9977869
Testa Stefano <s.testa@vallauri.edu>
2025-09-19 10:09:15 +0200

    Aggiunte Controlli clsAlunni

--- ES02_MVC_S.TESTA/Model/clsAlunni.cs
+++ ES02_MVC_S.TESTA/Model/clsAlunni.cs
@@ -9,5 +9,69 @@ namespace ES02_MVC_S.TESTA.Model
     internal class clsAlunni
     {
         private int idAlunno;
+        private string cognome;
+        private string nome;
+        private DateTime dataNascita;
+        private bool icdl;
+
+        public int IdAlunno
+        {
+            get
+            {
+                return idAlunno; 
+            }
+            set
+            {
+                idAlunno = value;
+            }
+        }
+
+        public string Cognome
+        {
+            get
+            {
+                return cognome;
+            }
+            set
+            {
+                cognome = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public DateTime DataNascita
+        {
+            get
+            {
+                return dataNascita;
+            }
+            set
+            {
+                dataNascita = value;
+            }
+        }
+
+        public bool Icdl
+        {
+            get
+            {
+                return icdl;
+            }
+            set
+            {
+                icdl = value;
+            }
+        }
     }
 }
